999bc286c0a249485e7c64c97aa7043b0a1e34b8
Saifur rahman sami <saifurrahmansami@gmail.com>
2024-09-30 16:10:34 +0600

    Generate fake data and insert to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Book::truncate();
+        Book::factory()->count(1000)->create();
     }
 }
