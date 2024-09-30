fd920586200cfe9f49bac9b5294dde785b44da9b
Saifur rahman sami <saifurrahmansami@gmail.com>
2024-09-30 21:55:05 +0600

    create book, added common layout

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -23,4 +23,25 @@ class BookController extends Controller
 
         return view("books.show")->with("book", $book);
     }
+    public function create()
+    {
+        return view("books.create");
+    }
+    public function store(Request $request)
+    {
+        // dd($request->all());
+
+        $rules = [
+            "title"=> "required",
+            "author"=> "required",
+            "isbn"=> "required|size:13",
+            "stock"=> "required|numeric|integer|gte:0",
+            "price"=> "required|numeric|gte:0",
+        ];
+        $request->validate($rules);
+
+        // the attribute name must be identical with database attributes name. Only then following line will work.
+        $book = Book::create($request->all());
+        return redirect()->route("books.show", $book->id);
+    }
 }
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,15 +1,13 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('books.layout')
+
+
+@section('page-content')
+
     
     <a href="{{route('books.index')}}">Back</a>
     <!-- {{$book}} -->
 
-    <table border="1" width="300">
+    <table class="table table-striped table-bordered">
         <tr>
             <th>ID</th>
             <td>{{$book->id}}</td>
@@ -35,6 +33,5 @@
             <td>{{$book->price}}</td>
         </tr>
     </table>
-
-</body>
-</html>
+    
+@endsection
