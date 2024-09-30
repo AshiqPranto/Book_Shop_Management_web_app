209d0c37f8bfbc04625e6435d411d6cd20b4bb80
Saifur rahman sami <saifurrahmansami@gmail.com>
2024-09-30 17:02:23 +0600

    Created router and controler and view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        // return 'Hello from index';
+
+        //load data from database
+        $books = Book::whereBetween('price', [10, 15])
+                ->where('stock', '>', 0)
+                ->orderBy('price')
+                ->get();
+        
+        return view("books.index")->with("books", $books);
+    }
 }
