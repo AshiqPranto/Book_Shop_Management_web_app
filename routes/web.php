fc6b1d598ff37e0440536d6f0e6920b9fb5ceaad
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2024-10-01 10:23:56 +0600

    route update

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,9 @@ Route::get('/books/{book}/show', [BookController::class,'show'])->name("books.sh
 Route::get("books/create", [BookController::class, 'create'])->name('books.create');
 Route::post("/books", [BookController::class,"store"])->name("books.store");
 Route::delete("/books/{id}", [BookController::class,"destroy"])->name("books.destroy");
-Route::edit()
+
+Route::get('/employee/{id}/update', [EmployeeController::class, 'edit'])->name('employee.edit');
+Route::post('/employee/{id}/update', [EmployeeController::class, 'update'])->name('employee.update');
+Route::get('/employee/search', [EmployeeController::class, 'search'])->name('employee.search');
+
 
